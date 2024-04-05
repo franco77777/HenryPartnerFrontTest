e8d3714726338e8ad1cb75288a4921134ac5694d
Franco Vedia <francoaresis@gmail.com>
2024-04-05 12:28:47 -0300

    input quantity fixed

--- src/components/paginate/Pagination.tsx
+++ src/components/paginate/Pagination.tsx
@@ -65,6 +65,7 @@ export default function PaginationComponent() {
 
   const changingCurrentQuantity = (e: string) => {
     dispatch(setCurrentQuantity(parseInt(e)));
+    dispatch(setCurrentPage(1));
   };
 
   return (
